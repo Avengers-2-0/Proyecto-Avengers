5582aff215f3d6884b4e44a281a8d241fbacb1d3
Miguel Angel Madera Ricardo <migueanmary@hotmail.com>
2022-08-20 23:31:13 -0500

    Se agregan atributos,metodo constructor, getters and setters, to String, a la clase empresa

--- Empresa.java
+++ Empresa.java
@@ -1,2 +1,58 @@
 public class Empresa {
+    public String nombreEmpresa;
+    public String direccionEmpresa;
+    public int telefonoEmpresa;
+    public String nitEmpresa;
+
+
+    public Empresa(String nombreEmpresa, String direccionEmpresa, int telefonoEmpresa, String nitEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+        this.direccionEmpresa = direccionEmpresa;
+        this.telefonoEmpresa = telefonoEmpresa;
+        this.nitEmpresa = nitEmpresa;
+
+    }
+
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getDireccionEmpresa() {
+        return direccionEmpresa;
+    }
+
+    public void setDireccionEmpresa(String direccionEmpresa) {
+        this.direccionEmpresa = direccionEmpresa;
+    }
+
+    public int getTelefonoEmpresa() {
+        return telefonoEmpresa;
+    }
+
+    public void setTelefonoEmpresa(int telefonoEmpresa) {
+        this.telefonoEmpresa = telefonoEmpresa;
+    }
+
+    public String getNitEmpresa() {
+        return nitEmpresa;
+    }
+
+    public void setNitEmpresa(String nitEmpresa) {
+        this.nitEmpresa = nitEmpresa;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "empresa{" +
+                "nombreEmpresa='" + nombreEmpresa + '\'' +
+                ", direccionEmpresa='" + direccionEmpresa + '\'' +
+                ", telefonoEmpresa=" + telefonoEmpresa +
+                ", nitEmpresa='" + nitEmpresa + '\'' +
+                '}';
+    }
 }
