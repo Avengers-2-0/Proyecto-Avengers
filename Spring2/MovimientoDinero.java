a34334b290a59f7f01a3dc6d3abfade1abb29308
AN87dres <107963838+AN87dres@users.noreply.github.com>
2022-08-27 09:55:53 -0500

    Update MovimientoDinero.java
    agrego cambios metodos y atributos

--- Spring2/MovimientoDinero.java
+++ Spring2/MovimientoDinero.java
@@ -1,5 +1,38 @@
 package Spring2;
 
 public class MovimientoDinero {
+   public int montoMovimiento;
+    public String conceptoMovimiento;
+    public String usuarioMoviento;
+
+    public MovimientoDinero(int montoMovimiento, String conceptoMovimiento, String usuarioMoviento) {
+        this.montoMovimiento = montoMovimiento;
+        this.conceptoMovimiento = conceptoMovimiento;
+        this.usuarioMoviento = usuarioMoviento;
+    }
+
+    public int getMontoMovimiento() {
+        return montoMovimiento;
+    }
+
+    public void setMontoMovimiento(int montoMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+    }
+
+    public String getConceptoMovimiento() {
+        return conceptoMovimiento;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    public String getUsuarioMoviento() {
+        return usuarioMoviento;
+    }
+
+    public void setUsuarioMoviento(String usuarioMoviento) {
+        this.usuarioMoviento = usuarioMoviento;
+    }
 
 }
