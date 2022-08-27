52d18c0930db2bdae7b20920b0249863c67cbb36
Anyell Gisell Perez Dominguez <111400044+anyperez93@users.noreply.github.com>
2022-08-27 10:03:26 -0500

    Add files via upload
    Empleado

--- Spring2/Empleado.java
+++ Spring2/Empleado.java
@@ -1,7 +1,49 @@
 package Spring2;
-
-
-
+<<<<<<< HEAD
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Empleado {
+    private String nombreEmpleado;
+    private String correoEmpleado;
+    private String empresaEmpleado;
+
+    String [] rolEmpleado = new String[] {"Administrador","Operario"};
+
+
+    public Empleado(String nombreEmpleado, String correoEmpleado, String empresaEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+        this.correoEmpleado = correoEmpleado;
+        this.empresaEmpleado = empresaEmpleado;
+
+    }
+
+    public String getNombreEmpleado() {
+
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getCorreoEmpleado() {
+        return correoEmpleado;
+    }
+
+    public void setCorreoEmpleado(String correoEmpleado) {
+        this.correoEmpleado = correoEmpleado;
+    }
+
+    public String getEmpresaEmpleado() {
+        return empresaEmpleado;
+    }
+
+    public void setEmpresaEmpleado(String empresaEmpleado) {
+        this.empresaEmpleado = empresaEmpleado;
+    }
+=======
+
+public class Empleado {
+>>>>>>> feb0b96427ee985c8d57688952a3e1269a4b85df
 }
