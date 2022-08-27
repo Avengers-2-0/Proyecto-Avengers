b4185fe3f1f8017ce75565ee508c66a3473f6e2a
Miguel Angel Madera Ricardo <migueanmary@hotmail.com>
2022-08-27 09:59:16 -0500

    cambios anyell

--- Spring2/Empleado.java
+++ Spring2/Empleado.java
@@ -1,45 +1,7 @@
 package Spring2;
 
-import java.lang.reflect.Array;
-import java.util.*;
+
+
 
 public class Empleado {
-    private String nombreEmpleado;
-    private String correoEmpleado;
-    private String empresaEmpleado;
-
-    String [] rolEmpleado = new String[] {"Administrador","Operario"};
-
-
-    public Empleado(String nombreEmpleado, String correoEmpleado, String empresaEmpleado) {
-        this.nombreEmpleado = nombreEmpleado;
-        this.correoEmpleado = correoEmpleado;
-        this.empresaEmpleado = empresaEmpleado;
-
-    }
-
-    public String getNombreEmpleado() {
-
-        return nombreEmpleado;
-    }
-
-    public void setNombreEmpleado(String nombreEmpleado) {
-        this.nombreEmpleado = nombreEmpleado;
-    }
-
-    public String getCorreoEmpleado() {
-        return correoEmpleado;
-    }
-
-    public void setCorreoEmpleado(String correoEmpleado) {
-        this.correoEmpleado = correoEmpleado;
-    }
-
-    public String getEmpresaEmpleado() {
-        return empresaEmpleado;
-    }
-
-    public void setEmpresaEmpleado(String empresaEmpleado) {
-        this.empresaEmpleado = empresaEmpleado;
-    }
 }
