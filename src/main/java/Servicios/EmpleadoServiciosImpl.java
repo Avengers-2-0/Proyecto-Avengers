b465677ef7e7624d049e5461b7aa15f13086bd8e
Anyell Gisell Perez Dominguez <111400044+anyperez93@users.noreply.github.com>
2022-09-10 13:11:58 -0500

    Update EmpleadoServiciosImpl.java
    se realiza la implementacion del servicio

--- src/main/java/Servicios/EmpleadoServiciosImpl.java
+++ src/main/java/Servicios/EmpleadoServiciosImpl.java
@@ -8,30 +8,50 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class EmpleadoServiciosImpl implements EmpleadoServicios {
+    
     @Autowired
     EmpleadoRepositorio empleadoRepositorio;
     @Override
     public List<Empleado> getEmpleados() throws Exception{
-        return null;
+        return this.empleadoRepositorio.findAll();
     }
 
     @Override
     public Empleado getEmpleado(Long id) throws Exception {
-        return null;
+        Optional<Empleado> empresa = this.empleadoRepositorio.findById(id);
+        return empresa.orElse(null);
     }
 
     @Override
     public Empleado createEmpleado(Empleado empleado) {
-        return null;
+
+        return this.empleadoRepositorio.save(empleado);
     }
 
     @Override
     public Empleado updateEmpleado(Long id, Empleado empleado) throws Exception {
+
+        Optional<Empleado> dbEmpleado = this.empleadoRepositorio.findById(id);
+        if (dbEmpleado.isPresent()) {
+            Empleado empleado1=dbEmpleado.get();
+            empleado1.setNombreEmpleado(empleado1.getNombreEmpleado());
+            empleado1.setCorreoEmpleado(empleado1.getCorreoEmpleado());
+            empleado1.setRolEmpleado(empleado1.getRolEmpleado());
+            empleado1.setEmpresa(empleado1.getEmpresa());
+            this.empleadoRepositorio.save(empleado1);
+            return empleado1;
+
+        }
         return null;
     }
 
     @Override
     public boolean deleteEmpleado(Long id) throws Exception {
-        return false;
+        try {
+            this.empleadoRepositorio.deleteById(id);
+            return true;
+        } catch (Exception empleado1) {
+            return false;
+        }
     }
 }
