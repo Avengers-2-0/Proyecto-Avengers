c653e118a8aa51ce4ebbfd14418adbf90521ffb5
Anyell Gisell Perez Dominguez <111400044+anyperez93@users.noreply.github.com>
2022-09-10 13:05:24 -0500

    Update EmpleadoControladores.java
    Se configura los controladores empleado

--- src/main/java/Controladores/EmpleadoControladores.java
+++ src/main/java/Controladores/EmpleadoControladores.java
@@ -1,4 +1,41 @@
 package Controladores;
 
+import Entidades.Empleado;
+import Servicios.EmpleadoServicios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("Empleados")
+
 public class EmpleadoControladores {
+  @Autowired
+    EmpleadoServicios servicios;
+
+    @GetMapping("/empleados")
+    public List<Empleado> getEmpleados(){
+        return servicios.getEmpleados();
+    }
+
+    @GetMapping("/empleado/{id}")
+    public Empleado getEmpleado(@PathVariable("id") Long id) throws Exception {
+        return servicios.getEmpleado(id);
+    }
+
+    @PostMapping("/empleado/new")
+    public Empleado createEmpleado(@RequestBody Empleado empleado){
+        return servicios.createEmpleado(empleado);
+    }
+
+   @PutMapping("/empleado/{id}")
+    public Empleado updateEmpleado(@PathVariable("id") Long id, @RequestBody Empleado empleado) throws Exception{
+        return servicios.updateEmpleado(id,empleado);
+   }
+
+    @DeleteMapping("/empleado/{id}")
+    public boolean deleteEmpleado(@PathVariable("id") Long id) throws Exception{
+        return servicios.deleteEmpleado(id);
+    }
 }
