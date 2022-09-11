132efd365c17c7e98dd4abe7d64adc47fb4fad5e
Miguel Angel Madera Ricardo <migueanmary@hotmail.com>
2022-09-11 17:43:55 -0500

    actualizaciones repositorios

--- src/main/java/Repositorios/MovimientoDineroRepositorio.java
+++ src/main/java/Repositorios/MovimientoDineroRepositorio.java
@@ -2,6 +2,8 @@ package Repositorios;
 
 import Entidades.MovimientoDinero;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovimientoDineroRepositorio extends JpaRepository<MovimientoDinero, Long> {
 }
