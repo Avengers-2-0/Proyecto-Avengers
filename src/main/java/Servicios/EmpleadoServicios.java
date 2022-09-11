109168f9f3a2d6015499c69d94d4ca9875928be2
Miguel Angel Madera Ricardo <migueanmary@hotmail.com>
2022-09-11 18:35:50 -0500

    cambios

--- src/main/java/Servicios/EmpleadoServicios.java
+++ src/main/java/Servicios/EmpleadoServicios.java
@@ -5,14 +5,14 @@ import Entidades.Empleado;
 import java.util.List;
 
 public interface EmpleadoServicios {
-    public List<Empleado> getEmpleados()throws Exception;
+    List<Empleado> getEmpleados()throws Exception;
 
-    public Empleado getEmpleado(Long id) throws Exception;
+    Empleado getEmpleado(Long id) throws Exception;
 
-    public Empleado createEmpleado(Empleado empleado);
+    Empleado createEmpleado(Empleado empleado);
 
-    public Empleado updateEmpleado(Long id, Empleado empleado) throws Exception;
+    Empleado updateEmpleado(Long id, Empleado empleado) throws Exception;
 
-    public boolean deleteEmpleado(Long id) throws Exception;
+    boolean deleteEmpleado(Long id) throws Exception;
 
 }
